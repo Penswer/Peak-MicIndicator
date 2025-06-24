832f454eaf76712a0c805e11c3dffe704bfc16d1
Penswer <penswer@gmail.com>
2025-06-24 12:06:32 -0400

    Did the stuff

--- EventComponent.cs
+++ EventComponent.cs
@@ -1,15 +1,68 @@
 using UnityEngine;
 using System;
 using MicIndicator;
+using Photon.Voice.PUN;
+using UnityEngine.UI;
 
 public class EventComponent : MonoBehaviour
 {
+    RawImage micIcon = null;
+    PhotonVoiceView micView = null;
+
     void Update()
     {
-        if (Input.GetKeyDown(Plugin.configKeyBind.Value))
-        {
-        }
 
+        if (Plugin.itsTimeXD)
+        {
+            if (micIcon == null)
+            {
+                var micIconLocal = GameObject.Find("/GAME/GUIManager/Canvas_HUD/Inventory/Layout/UI_InventoryBackpack/Icon/");
+                var micIconLocalParent = GameObject.Find("/GAME/GUIManager/Canvas_HUD/");
+                if (micIconLocal != null && micIconLocalParent != null)
+                {
+                    // micIcon = micIconLocal;
+                    micIconLocal = GameObject.Instantiate(micIconLocal);
+                    micIconLocal.transform.parent = micIconLocalParent.transform;
+                    micIconLocal.transform.localScale = new Vector3(Plugin.configScale.Value, Plugin.configScale.Value, Plugin.configScale.Value);
+                    micIconLocal.transform.localPosition = new Vector3(Plugin.configPosition.Value.x, Plugin.configPosition.Value.y, 0.0f);
+
+                    // micIconLocal.SetActive(Plugin.configIsActive.Value);
+                    RawImage micIconImageLocal = micIconLocal.GetComponent<RawImage>();
+                    if (micIconImageLocal != null)
+                    {
+                        micIcon = micIconImageLocal;
+                        micIcon.enabled = Plugin.configIsActive.Value;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(Plugin.configToggleKey.Value))
+                {
+                    Plugin.configIsActive.Value ^= true;
+                    micIcon.enabled = Plugin.configIsActive.Value;
+                }
+                if (micView != null)
+                {
+                    if (micView.IsRecording)
+                    {
+                        micIcon.texture = Plugin.micOnTex;
+                    }
+                    else
+                    {
+                        micIcon.texture = Plugin.micOffTex;
+                    }
+                }
+                else
+                {
+                    var view = Character.localCharacter?.GetComponent<PhotonVoiceView>();
+                    if (view != null)
+                    {
+                        micView = view;
+                    }
+                }
+            }
+        }
     }
 
     void OnGUI()
@@ -22,10 +75,6 @@ public class EventComponent : MonoBehaviour
 
     void Start()
     {
-        Action act = () =>
-        {
-        };
-        // this.StartCoroutine(ButtonAPI.WaitForQMClone(act).WrapToIl2Cpp());
     }
 
 }
--- Plugin.cs
+++ Plugin.cs
@@ -9,6 +9,14 @@ using Zorro.Core.Serizalization;
 using pworld.Scripts;
 using HarmonyLib;
 using BepInEx.Configuration;
+using UnityEngine.UI;
+using System.Reflection;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using ExitGames.Client.Photon.StructWrapping;
+using Sirenix.Utilities;
+using Unity.Collections;
 
 namespace MicIndicator;
 
@@ -17,13 +25,24 @@ public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
     internal static EventComponent Component;
-    
-    public static ConfigEntry<KeyCode> configKeyBind;
+
+    public static ConfigEntry<KeyCode> configToggleKey;
+    public static ConfigEntry<bool> configIsActive;
+    public static ConfigEntry<Vector2> configPosition;
+    public static ConfigEntry<float> configScale;
+
+    internal static Texture2D micOnTex = null;
+    internal static Texture2D micOffTex = null;
+
+    internal static bool itsTimeXD = false;
 
     private void Awake()
     {
         // Plugin startup logic
-        configKeyBind = Config.Bind("Keybinds", "ToggleKey", KeyCode.Home, "The key used to toggle the GUI on and off");
+        configToggleKey = Config.Bind("Keybinds", "ToggleKey", KeyCode.End, "The key used to toggle the GUI on and off");
+        configIsActive = Config.Bind("GUI", "IsActive", true, "If the icon is showing");
+        configPosition = Config.Bind("GUI", "Position", new Vector2(-920f, -370f), "The position of the icon");
+        configScale = Config.Bind("GUI", "Scale", 5.0f, "The scale of the icon");
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         foreach (var option in Environment.GetCommandLineArgs())
@@ -31,6 +50,32 @@ public class Plugin : BaseUnityPlugin
             Logger.LogInfo($"Launch Options {option}.!");
         }
         Component = this.gameObject.AddComponent<EventComponent>();
+        micOnTex = GetTextureFromAssemImage("MicIndicator.Images.MicOn.png");
+        micOffTex = GetTextureFromAssemImage("MicIndicator.Images.MicOff.png");
+        itsTimeXD = true;
+        // micIcon.GetComponent<RawImage>()
+    }
+
+    internal Texture2D GetTextureFromAssemImage(string imageName)
+    {
+        // Assembly.GetExecutingAssembly().GetManifestResourceNames().ForEach((x) => { Logger.LogError(x); });
+        using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(imageName))
+        {
+            if (stream == null)
+            {
+                Logger.LogError("COULD NOT FIND MIC ICON. REPORT THIS TO MOD MAKER");
+                return null;
+            }
+            var image = System.Drawing.Image.FromStream(stream);
+            Texture2D tex = new Texture2D(image.Width, image.Height);
+            using (var memStream = new MemoryStream())
+            {
+                image.Save(memStream, ImageFormat.Png);
+                tex.LoadImage(memStream.ToArray());
+                tex.Apply();
+            }
+            return tex;
+        }
     }
 
 }
